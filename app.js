06ed69349ea66fefebcb370e8f08a236933c5976
nateos <sodjanathan@gmail.com>
2021-04-03 18:55:05 +0000

    Assigning date values to html

--- app.js
+++ app.js
@@ -21,3 +21,28 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway')
+const deadline = document.querySelector('.deadline')
+const timeItems = document.querySelectorAll('.deadline h4')
+
+let futureDate = new Date(2021, 4, 24, 11, 30, 0)
+console.log(futureDate);
+
+const year = futureDate.getFullYear()
+const hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+
+let month = futureDate.getMonth()
+month = months[month]
+
+let day = futureDate.getDay()
+day = weekdays[day]
+
+let date = futureDate.getDate()
+
+
+
+giveaway.textContent = `giveaway ends on ${day}, ${date}th ${month} ${year},  at ${hours}: ${minutes} GMT`
+
+
